6114b0b8c72d8b9e1dc894802326588057bb5507
SHAKIB <143376869+terafazel@users.noreply.github.com>
2025-02-11 19:05:42 +0530

    Update streamlit_app.py
    added new sf_df frame for at the last to show the data table

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,6 @@ if ingredients_list:
 #new section to display the smoothie fruit nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
+#st.text(smoothiefroot_response)
+sf_df =st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
