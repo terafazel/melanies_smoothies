93b71ffed1a2d5e10d9861ab24f6f9632d1e522e
SHAKIB <143376869+terafazel@users.noreply.github.com>
2025-02-11 19:10:51 +0530

    Update streamlit_app.py
    added the import statement at the end of line 4 and aslo moved the last line of 
    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
    sf_df =st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
    
    at for fruit_chosen in ingredients

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Smoothie :cup_with_straw:")
@@ -36,6 +37,8 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ''
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df =st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     st.write( ingredients_string)
     
@@ -53,9 +56,6 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered! ' + name_on_order, icon="✅")
 
-#new section to display the smoothie fruit nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response)
-sf_df =st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
 
