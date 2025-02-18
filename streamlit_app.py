3ba11a15f29e23f50a7a9ba6a5ac618d2fc96066
SHAKIB <143376869+terafazel@users.noreply.github.com>
2025-02-18 18:57:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,28 +15,23 @@ name_on_order = st.text_input("The Name on the Smoothie : ")
 st.write("Smoothie Name : ", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True) #st is used to display the said content 
+# st.dataframe(data=my_dataframe, use_container_width=True) # st is used to display the said content 
 
 ingredients_list = st.multiselect(
-    "CHOOSE UPTO TO 5 INGREDIENTS",
+    "CHOOSE UP TO 5 INGREDIENTS",
     my_dataframe,
     max_selections=5 
 )
 
 if ingredients_list: 
-    # st.write("You selected:", ingredients_list) #
-    # st.text(ingredients_list)
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(name_on_order, ingredients)
-            values ('""" + name_on_order + """', '""" + ingredients_string + """')"""
+        values ('""" + name_on_order + """', '""" + ingredients_string + """')"""
 
-    # st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
